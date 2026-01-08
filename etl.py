f9632afc6d6b604aa67a23478125160bba4bcb1f
Michel BLAZEVIC <michel.blazevic@safrangroup.com>
2026-01-08 17:25:20 +0100

    add parameters to wait_engine inside etl.py

--- etl.py
+++ etl.py
@@ -1,13 +1,13 @@
 from config import get_config
 import os
 from database.models import Events
-from database.init_or_check_db import wait_for_db,get_database_url,get_tables_database
+from database.init_or_check_db import create_engine,wait_for_db,get_database_url,get_tables_database
 
 if __name__ == '__main__':
     conf = get_config()
     if os.getenv('DATABASE_URL'):
         print('DATABASE URL exist')
-        if wait_for_db():
+        if wait_for_db(create_engine(get_database_url())):
             print("let's continue")
             tables = get_tables_database()
             print(tables)
