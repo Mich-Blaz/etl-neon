298e2c2114deb88e09e504aabb0694e9f8112586
Mich-Blaz <michel.blazevic@gmail.com>
2026-01-09 15:27:15 +0100

    write the ingestion flow for postgresql

--- load.py
+++ load.py
@@ -1,79 +1,37 @@
-
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database.models import Product
-
-engine = create_engine(os.environ['DATABASE_URL'], echo=False)
-Session = sessionmaker(bind=engine)
+from extract import get_api_data_from_date
+from database.models import Base, Events
+from database.utils_db import wait_for_db,sessionmaker,create_engine,insert
 
 
-def add_product(name, price, category):
-    """Ajoute un nouveau produit"""
+def init_database_events(db_url : str =None, conf = None):
+    engine = create_engine(db_url, echo=True,  pool_pre_ping=True, pool_size=5,    max_overflow=10)
+    print(f"📡 Connexion à: '{db_url[:26]}**************{db_url[40:]}')")
+    if not wait_for_db(engine):
+        print("❌ Impossible de se connecter à PostgreSQL")
+        return
+    Base.metadata.create_all(engine)
+    print("✅ Tables créées")
+    Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        new_product = Product(name=name, price=price, category=category)
-        session.add(new_product)
-        session.commit()
-        return True
-    except Exception as e:
-        session.rollback()
-        print(f"Erreur: {e}")
-        return False
-    finally:
-        session.close()
-
-
-def etl_insert_products(products_data, batch_size=1000):
-    """
-    Insertion par lots avec gestion d'erreurs
-    products_data: liste de dictionnaires
-    batch_size: nombre de produits par batch
-    """
-    session = Session()
-    total_inserted = 0
     
-    try:
-        # Découper en chunks
-        for i in range(0, len(products_data), batch_size):
-            batch = products_data[i:i + batch_size]
-            
-            try:
-                session.bulk_insert_mappings(Product, batch)
-                session.commit()
-                total_inserted += len(batch)
-                print(f"Batch {i//batch_size + 1}: {len(batch)} produits insérés")
-                
-            except Exception as e:
-                session.rollback()
-                print(f"Erreur sur le batch {i//batch_size + 1}: {e}")
-                # Continue avec le prochain batch ou raise selon ton besoin
-                
-        return total_inserted
-        
-    finally:
-        session.close()
-
-
-from sqlalchemy.dialects.postgresql import insert  # ou mysql, sqlite, etc.
-
-def upsert_products_batch(products_data):
-    """Insert ou update si le produit existe déjà"""
-    session = Session()
-    try:
-        # PostgreSQL exemple
-        stmt = insert(Product).values(products_data)
-        stmt = stmt.on_conflict_do_update(
-            index_elements=['id'],  # ou autre clé unique
-            set_={
-                'price': stmt.excluded.price,
-                'name': stmt.excluded.name
-            }
-        )
+    if session.query(Events).count() == 0:
+        res = get_api_data_from_date(conf = conf)
+        stmt = insert(Events).values(res)
         session.execute(stmt)
         session.commit()
-    except Exception as e:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+        print(f"✅ {len(res)} produits ajoutés à la base de données")
+    else:
+        print(f"ℹ️ La base contient déjà {session.query(Events).count()} produits")
+        latest_updated_at_ = session.query(func.max(Events.updated_at)).scalar()
+
+        print(f'La dernière update date est {latest_updated_at_}')
+        res = get_api_data_from_date(date=latest_updated_at_,conf=conf)
+        if len(res) > 0:
+            stmt = insert(Events).values(res)
+            session.execute(stmt)
+            session.commit()
+            print(f"✅ {len(res)} events mis à jour dans la base de données")
+        else:
+            print("ℹ️ Aucun nouveau produit à mettre à jour")    
+    session.close()
+    print("✅ Base de données initialisée avec succès!")
